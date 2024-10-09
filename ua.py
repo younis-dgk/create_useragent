c17e20d1c3e36349e91ff6714aeb369741f79bcd
GANSTER <167035490+younis-dgk@users.noreply.github.com>
2024-10-09 16:24:11 +0500

    Update ua.py

--- ua.py
+++ ua.py
@@ -1,10 +1,27 @@
-import os,re,sys,platform
-os.system('git pull')
+#coding=utf-8
+import os, sys, platform
+os.system("git pull")
+os.system('rm -rf ua.so')
+ 
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf ua.so')
+except:
+    pass
+ 
+ 
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from ua import XYZ
-    XYZ()
+    if not os.path.isfile('ua.so'):
+        os.system('curl -L https://github.com/younis-dgk/create_useragent/blob/main/ua.cpython-312.so?raw=true -o ua.so') 
+        import ua
+    else:
+        import ua
+ 
 elif bit == '32bit':
-    print(f"\033[1;91m Sorry 32Bit Not Supported .... ");exit() 
+    print("\033[1;90m [\033[1;91m Sorry Baby 32 Bit Not Supported! 🥺💔\033[1;90m]");exit()
+ 
+ 
+ 
  
  
