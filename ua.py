2109c6ea9cf83d43f0e0d52d161fa69aeb28eb26
GANSTER <167035490+younis-dgk@users.noreply.github.com>
2024-12-14 15:49:35 +0500

    Update ua.py

--- ua.py
+++ ua.py
@@ -1,27 +1,7 @@
-#coding=utf-8
-import os, sys, platform
-os.system("git pull")
-os.system('rm -rf ua.so')
- 
-try:
-    if sys.argv[1]=='update':
-        os.system('rm -rf ua.so')
-except:
-    pass
- 
- 
-bit = platform.architecture()[0]
-if bit == '64bit':
-    if not os.path.isfile('ua.so'):
-        os.system('curl -L https://github.com/younis-dgk/create_useragent/blob/main/ua.cpython-312.so?raw=true -o ua.so') 
-        import ua
-    else:
-        import ua
- 
-elif bit == '32bit':
-    print("\033[1;90m [\033[1;91m Sorry Baby 32 Bit Not Supported! 🥺💔\033[1;90m]");exit()
- 
- 
- 
- 
- 
+import os
+os.system("clear")
+x='\033[0;97mTool has been deleted permanently\nYounis john no mores updates on it'
+y='For More About Author : https://bio.link/younis_dgk'
+print(x)
+print(y)
+exit()
